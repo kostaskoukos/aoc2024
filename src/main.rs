bb2375ff34143c9dc61b28a038a1f12bfed19010
kostaskoukos <kkonst2014@gmail.com>
2024-12-07 15:28:57 +0200

    add simple day handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let day = std::env::args()
+        .nth(1)
+        .expect("Must provide a day of the challenge (dayN)");
+    println!("Running {}", day);
+
+    match day.as_str() {
+        "day1" => println!("{}", run_day1()),
+        _ => panic!("Couldn't indentify day!"),
+    }
+}
+
+fn run_day1() -> &'static str {
+    "pass"
 }
