d302e4d8e6fe120e196b1de270f24e4033677306
kostaskoukos <kkonst2014@gmail.com>
2024-12-08 18:22:10 +0200

    add day2 (part 2)

--- src/day2.rs
+++ src/day2.rs
@@ -3,19 +3,33 @@ use std::{
     io::{self, BufRead},
 };
 
-pub fn run_day2() -> usize {
-    io::BufReader::new(File::open("./input2.txt").unwrap())
-        .lines()
-        .filter(|line| {
-            let vec: Vec<u32> = line
-                .as_ref()
-                .unwrap()
-                .split_whitespace()
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect();
-            is_valid(&vec)
-        })
-        .count()
+pub fn run_day2() -> (usize, usize) {
+    (
+        io::BufReader::new(File::open("./input2.txt").unwrap())
+            .lines()
+            .filter(|line| {
+                let vec: Vec<u32> = line
+                    .as_ref()
+                    .unwrap()
+                    .split_whitespace()
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect();
+                is_valid(&vec)
+            })
+            .count(),
+        io::BufReader::new(File::open("./input2.txt").unwrap())
+            .lines()
+            .filter(|line| {
+                let vec: Vec<u32> = line
+                    .as_ref()
+                    .unwrap()
+                    .split_whitespace()
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect();
+                is_tolerable(&vec)
+            })
+            .count(),
+    )
 }
 
 fn is_valid(vec: &[u32]) -> bool {
@@ -25,4 +39,16 @@ fn is_valid(vec: &[u32]) -> bool {
     vec.windows(2)
         .map(|w| w[0].abs_diff(w[1]))
         .all(|v| (1..=3).contains(&v))
+}
+
+fn is_tolerable(vec: &[u32]) -> bool {
+    if is_valid(vec) {
+        return true;
+    }
+    for i in 0..vec.len() {
+        if is_valid(&[&vec[0..i], &vec[i + 1..]].concat()) {
+            return true;
+        };
+    }
+    false
 }
