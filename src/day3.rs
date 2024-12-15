07c7b74f7d3122c1bb7d3092451091c147a83117
kostaskoukos <kkonst2014@gmail.com>
2024-12-15 15:10:32 +0200

    add day3 (part 2)

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,11 @@
 use regex::Regex;
 use std::fs;
 
-pub fn run_day3() -> u32 {
+pub fn run_day3() -> (u32, u32) {
+    (part1(), part2())
+}
+
+fn part1() -> u32 {
     Regex::new(r"mul\((?<x>[0-9]+),(?<y>[0-9]+)\)")
         .unwrap()
         .captures_iter(fs::read_to_string("./input3.txt").unwrap().as_str())
@@ -9,4 +13,23 @@ pub fn run_day3() -> u32 {
         .map(|(_, [x, y])| (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap()))
         .map(|(x, y)| x * y)
         .sum()
+}
+
+fn part2() -> u32 {
+    let mut valid = true;
+    let mut res = 0u32;
+    for c in Regex::new(r"mul\((?<x>[0-9]+),(?<y>[0-9]+)\)|(?<yes>do\(\))|(?<no>don't\(\))")
+        .unwrap()
+        .captures_iter(fs::read_to_string("./input3.txt").unwrap().as_str())
+    {
+        if c.name("yes").is_some() {
+            valid = true;
+        } else if c.name("no").is_some() {
+            valid = false;
+        } else if valid {
+            res += c.name("x").unwrap().as_str().parse::<u32>().unwrap()
+                * c.name("y").unwrap().as_str().parse::<u32>().unwrap()
+        }
+    }
+    res
 }
