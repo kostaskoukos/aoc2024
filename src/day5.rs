14e82b05f8d5d3356c4d4da19626ddf84a1a7127
kostaskoukos <kkonst2014@gmail.com>
2024-12-25 17:16:33 +0200

    add day5 (part 1)

---
+++ src/day5.rs
@@ -0,0 +1,39 @@
+use std::{
+    cmp::Ordering,
+    collections::HashMap,
+    fs::{self},
+};
+
+pub fn run_day5() -> (usize, usize) {
+    let input = fs::read_to_string("input5.txt").unwrap();
+    let (order, list) = input.split_once("\n\n").unwrap();
+
+    let order: Vec<(u32, u32)> = order
+        .lines()
+        .map(|l| l.split_once("|").unwrap())
+        .map(|(a, b)| (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap()))
+        .collect();
+
+    let mut updates: Vec<Vec<u32>> = list
+        .lines()
+        .map(|l| l.split(",").map(|num| num.parse().unwrap()).collect())
+        .collect();
+
+    (part1(&updates, &order), part2(&mut updates, &order))
+}
+
+fn part1(updates: &[Vec<u32>], order: &[(u32, u32)]) -> usize {
+    updates
+        .iter()
+        .filter(|update| {
+            let update_map: HashMap<u32, usize> =
+                update.iter().enumerate().map(|(i, &v)| (v, i)).collect();
+            !order.iter().any(|(a, b)| {
+                update_map.contains_key(a)
+                    && update_map.contains_key(b)
+                    && update_map.get(a).unwrap() > update_map.get(b).unwrap()
+            })
+        })
+        .map(|update| update[update.len() / 2] as usize)
+        .sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use crate::day3::run_day3;
 mod day4;
 use crate::day4::run_day4;
 
+mod day5;
+use crate::day5::run_day5;
+
 fn main() {
     let day = std::env::args()
         .nth(1)
@@ -21,6 +24,7 @@ fn main() {
         "day2" => println!("{:?}", run_day2()),
         "day3" => println!("{:?}", run_day3()),
         "day4" => println!("{:?}", run_day4()),
+        "day5" => println!("{:?}", run_day5()),
         _ => panic!("Couldn't indentify day!"),
     }
 }
